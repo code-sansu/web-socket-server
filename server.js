5e919d78cd6d5264bd6d33d19dfd8b395f19ee95
Code-Sansu <sanskarsharma260801@gmail.com>
2025-04-28 16:58:51 +0530

    updates

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
+const express = require('express');
+const WebSocket = require('ws');
+const path = require('path');
+const http = require('http');
+const { handleConnection } = require("./Services/websocketServices.js");
+const { logMessage } = require("./Services/logServices.js");
 
+const app = express();
+
+// Serve static files from the React app (assuming the build is inside 'client/build')
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle WebSocket connections
+const server = http.createServer(app); // Use the same server instance for both Express and WebSocket
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => handleConnection(ws, wss));
+
+// Catch-all handler for all routes to return the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`WebSocket server is listening on port ${PORT}`);
+  logMessage(`Server started on port ${PORT}`);
+});
+
+/*
 const WebSocket = require("ws");
 const http = require('http');
 const { handleConnection } = require("./Services/websocketServices.js");
@@ -19,3 +49,4 @@ server.listen(PORT, "0.0.0.0" , () => {
     console.log(`WebSocket server is listening on port ${PORT}`);
     logMessage(`Server started on port ${PORT}`);
 });
+*/
