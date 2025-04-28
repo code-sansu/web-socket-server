106de4fecaa43700113c2805891373f65dd6d9b9
Code-Sansu <sanskarsharma260801@gmail.com>
2025-04-28 15:12:20 +0530

    Ensure vite is installed and clear cache

--- client/src/context/WebSocketContext.jsx
+++ client/src/context/WebSocketContext.jsx
@@ -20,8 +20,8 @@ const WebSocketContext = createContext();
   const socketRef = useRef(null);
 
   useEffect(() => {
-    
-    const ws = new WebSocket('import.meta.env.VITE_WS_URL');
+    const wsURL = import.meta.env.VITE_WS_URL;
+    const ws = new WebSocket(wsURL);
     socketRef.current = ws;
     setSocket(ws);
      
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
-  plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-  },
+  plugins: [react()]
 })
