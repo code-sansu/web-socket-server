1013505df59caa4d41a5c9c3563446e5207cf774
Code-Sansu <sanskarsharma260801@gmail.com>
2025-04-28 17:04:07 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,53 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import ServerPage from './components/ServerPage';
+import ClientPage from './components/ClientPage';
+import LogsPage from './LogsPage';
+import SamplePage from './SamplePage';
+import Dashboard from './components/Dashboard';
+import TestPage from './TestPage';
+import { WebSocketProvider } from './context/WebSocketContext';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
 
+        <Route
+          path="/client/*"
+          element={
+            <WebSocketProvider role="client">
+              <Routes>
+                <Route path="" element={<ClientPage />} />
+                <Route path="test" element={<TestPage />} />
+              </Routes>
+            </WebSocketProvider>
+          }
+        />
+
+        <Route
+          path="/server/*"
+          element={
+            <WebSocketProvider role="server">
+              <Routes>
+                <Route path="" element={<ServerPage />} />
+                <Route path="logs" element={<LogsPage />} />
+                <Route path="logs/sample" element={<SamplePage />} />
+                <Route path="dashboard" element={<Dashboard />} />
+              </Routes>
+            </WebSocketProvider>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+/*
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 import HomePage from './HomePage';
@@ -56,3 +104,4 @@ function App() {
 }
 
 export default App;
+*/
